653e6b769e7d78f41a8cbeda82a1a0d2ef7377b1
KO1012 <d6f6br7s7x@privaterelay.appleid.com>
2026-04-19 20:27:41 +0800

    Add lexcapital adapter utils wrapper

---
+++ src/lexcapital/adapters/utils.py
@@ -0,0 +1,3 @@
+from finrulebench.adapters.utils import default_hold_decision, extract_json_payload, parse_model_decision
+
+__all__ = ["default_hold_decision", "extract_json_payload", "parse_model_decision"]
